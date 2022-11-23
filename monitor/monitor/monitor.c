3b3cd2f59a791f81c542b11d60e0fc960ddd2ec4
Rustam Subkhankulov <subkhankulov@ispras.ru>
2022-11-24 01:41:31 +0300

    added monitor proto

--- monitor/monitor/monitor.c
+++ monitor/monitor/monitor.c
@@ -6,4 +6,77 @@
 
 //=========================================================
 
+static int monitor_checker(const struct Monitor* monitor)
+{
+    assert(monitor);
 
+    return MONITOR_OK;    
+}
+
+//=========================================================
+
+int monitor_ctor(struct Monitor* monitor)
+{
+    assert(monitor);
+
+    int err = 0;
+
+    err = rbuffer_ctor(&(monitor->rbuffer));
+    if (err != RBUFFER_OK)
+        return err;
+
+    return MONITOR_OK;
+}
+
+//---------------------------------------------------------
+
+int monitor_dtor(struct Monitor* monitor)
+{
+    assert(monitor);
+
+    int err = 0;
+
+    err = rbuffer_dtor(&(monitor->rbuffer));
+    if (err != RBUFFER_OK)
+        return err;
+
+    return MONITOR_OK;
+}
+
+//---------------------------------------------------------
+
+int monitor_read(struct Monitor* monitor, size_t size, void* addr)
+{
+    assert(monitor);
+
+    int err = 0;
+
+    // prologue
+
+    err = rbuffer_read(&(monitor->rbuffer), size, addr);
+    if (err != RBUFFER_OK)
+        return err;
+    
+    // epilogue
+
+    return MONITOR_OK;
+}
+
+//---------------------------------------------------------
+
+int monitor_write(struct Monitor* monitor, size_t size, void* addr)
+{
+    assert(monitor);
+
+    int err = 0;
+
+    // prologue
+
+    err = rbuffer_write(&(monitor->rbuffer), size, addr);
+    if (err != RBUFFER_OK)
+        return err;
+
+    // epilogue
+
+    return MONITOR_OK;
+}
--- monitor/monitor/monitor.h
+++ monitor/monitor/monitor.h
@@ -6,3 +6,26 @@
 
 //=========================================================
 
+struct Monitor
+{
+    struct Rbuffer rbuffer;
+};
+
+//---------------------------------------------------------
+
+enum Monitor_op_result
+{
+    MONITOR_OK = 0,
+    MONITOR_IV = -1,
+};
+
+//=========================================================
+
+int monitor_ctor(struct Monitor* monitor);
+
+int monitor_dtor(struct Monitor* monitor);
+
+int monitor_read(struct Monitor* monitor, size_t size, void* addr);
+
+int monitor_write(struct Monitor* monitor, size_t size, void* addr);
+
