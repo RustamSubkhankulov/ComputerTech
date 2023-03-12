11ac56f1225227866233ba9aa4d387401dcedd0b
Rustam Subkhankulov <subkhankulov@ispras.ru>
2023-03-12 03:09:25 +0300

    add graphics, sequencer & attribute controller vga registers support

--- itask/kern/vga_regs.c
+++ itask/kern/vga_regs.c
@@ -21,34 +21,274 @@ void set_graphics_reg(enum Graphic_register reg, uint8_t value)
     outb(GRAPH_CTRL_DATA_REG, value);
 }
 
-void graphics_reg_set_reset_set_bit(uint8_t bitno)
+void graphics_reg_set_bit(enum Graphic_register reg, uint8_t bitno)
 {
-    assert(bitno < 4);
+    assert(reg >= SET_RESET && reg <= BIT_MASK);
 
-    uint8_t set_reset = get_graphics_reg(SET_RESET);
-    set_graphics_reg(SET_RESET, set_reset | (1 << bitno));
+    uint8_t value = get_graphics_reg(reg);
+    set_graphics_reg(reg, value | (1 << bitno));
+    return;
 }
 
-void graphics_reg_set_reset_clear_bit(uint8_t bitno)
+void graphics_reg_clear_bit(enum Graphic_register reg, uint8_t bitno)
 {
-    assert(bitno < 4);
-
-    uint8_t set_reset = get_graphics_reg(SET_RESET);
-    set_graphics_reg(SET_RESET, set_reset & ((~(1 << bitno)) & 0b1111));
+    assert(reg >= SET_RESET && reg <= BIT_MASK);
+    
+    uint8_t value = get_graphics_reg(reg);
+    set_graphics_reg(reg, value & (~(1 << bitno)));
+    return;
 }
 
-void graphics_reg_enable_set_reset_set_bit(uint8_t bitno)
+bool graphics_reg_check_bit(enum Graphic_register reg, uint8_t bitno)
 {
-    assert(bitno < 4);
+    assert(reg >= SET_RESET && reg <= BIT_MASK);
 
-    uint8_t enable_set_reset = get_graphics_reg(ENABLE_SET_RESET);
-    set_graphics_reg(ENABLE_SET_RESET, enable_set_reset | (1 << bitno));
+    return (get_graphics_reg(reg) & (1 << bitno));
 }
 
-void graphics_reg_enable_set_reset_clear_bit(uint8_t bitno)
+uint8_t graphics_reg_data_rotate_logical_operation_get(void)
 {
-    assert(bitno < 4);
-
-    uint8_t enable_set_reset = get_graphics_reg(ENABLE_SET_RESET);
-    set_graphics_reg(ENABLE_SET_RESET, enable_set_reset & ((~(1 << bitno)) & 0b1111));
+    return (get_graphics_reg(DATA_ROTATE) >> 3) & 0b11;
 }
+
+void graphics_reg_data_rotate_logical_operation_set(uint8_t log_op)
+{
+    assert(log_op <= 0b11);
+
+    uint8_t data_rotate = get_graphics_reg(DATA_ROTATE);
+    set_graphics_reg(DATA_ROTATE, (data_rotate & 0b11100111) | (log_op << 3));
+    return;
+}
+
+void graphics_reg_data_rotate_rotate_count_set(uint8_t ct)
+{
+    assert(ct <= 0b111);
+
+    uint8_t data_rotate = get_graphics_reg(DATA_ROTATE);
+    set_graphics_reg(DATA_ROTATE, (data_rotate & 0b11111000) | ct);
+    return;
+}
+
+uint8_t graphics_reg_data_rotate_rotate_count_get(void)
+{
+    return get_graphics_reg(DATA_ROTATE) & 0b111;
+}
+
+void graphics_reg_read_map_select_set(uint8_t rms)
+{
+    assert(rms <= 0b11);
+
+    uint8_t read_map_select = get_graphics_reg(READ_MAP_SELECT);
+    set_graphics_reg(READ_MAP_SELECT, (read_map_select & 0b11111100) | rms);
+    return;
+}
+
+uint8_t graphics_reg_read_map_select_get(void)
+{
+    return get_graphics_reg(READ_MAP_SELECT) & 0b11;
+}
+
+void graphics_reg_graphics_mode_write_mode_set(uint8_t wmode)
+{
+    assert(wmode <= 0b11);
+
+    uint8_t graphics_mode = get_graphics_reg(GRAPHICS_MODE);
+    set_graphics_reg(GRAPHICS_MODE, (graphics_mode & 0b11111100) | wmode);
+}
+
+uint8_t graphics_reg_graphics_mode_write_mode_get(void)
+{
+    return get_graphics_reg(GRAPHICS_MODE) & 0b11;
+}
+
+void graphics_reg_misc_graphics_mem_map_select_set(uint8_t mms)
+{
+    assert(mms <= 0b11);
+
+    uint8_t misc_graphics = get_graphics_reg(MISC_GRAPHICS);
+    set_graphics_reg(MISC_GRAPHICS, (misc_graphics & 0b11110011) | mms);
+    return;
+}
+
+uint8_t graphics_reg_misc_graphics_mem_map_select_get(void)
+{
+    return (get_graphics_reg(MISC_GRAPHICS) >> 2) & 0b11;
+}
+
+uint8_t graphics_reg_clr_cmp_get(void)
+{
+    return get_graphics_reg(CLR_CMP) & 0b1111;
+}
+
+void graphics_reg_clr_cmp_set(uint8_t clr_cmp)
+{
+    assert(clr_cmp <= 0b1111);
+
+    uint8_t color_compare = get_graphics_reg(CLR_CMP);
+    set_graphics_reg(CLR_CMP, (color_compare & 0b11110000) | clr_cmp);
+    return;
+}
+
+uint8_t get_sequencer_reg(enum Sequencer_register reg)
+{
+    assert(reg >= RESET && reg <= SEQ_MEM_MODE);
+
+    outb(SEQ_ADDR_REG, reg);
+    return inb(SEQ_DATA_REG);
+}
+
+void set_sequencer_reg(enum Sequencer_register reg, uint8_t value)
+{
+    assert(reg >= RESET && reg <= SEQ_MEM_MODE);
+
+    outb(SEQ_ADDR_REG, reg);
+    outb(SEQ_DATA_REG, value);
+}
+
+void sequencer_reg_set_bit(enum Sequencer_register reg, uint8_t bitno)
+{
+    assert(reg >= RESET && reg <= SEQ_MEM_MODE);
+
+    uint8_t value = get_sequencer_reg(reg);
+    set_sequencer_reg(reg, value | (1 << bitno));
+    return;
+}
+
+void sequencer_reg_clear_bit(enum Sequencer_register reg, uint8_t bitno)
+{
+    assert(reg >= RESET && reg <= SEQ_MEM_MODE);
+
+    uint8_t value = get_sequencer_reg(reg);
+    set_sequencer_reg(reg, value & (~(1 << bitno)));
+    return;
+}
+
+bool sequencer_reg_check_bit(enum Sequencer_register reg, uint8_t bitno)
+{
+    assert(reg >= RESET && reg <= SEQ_MEM_MODE);
+
+    return (get_sequencer_reg(reg) & (1 << bitno));
+}
+
+void sequencer_reg_csas_set(uint8_t csas)
+{
+    assert(csas <= 0b111);
+
+    if (csas & 0b100)
+        sequencer_reg_set_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSAS2);
+    else 
+        sequencer_reg_clear_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSAS2);
+
+    csas &= 0b11;
+
+    uint8_t char_map_select = get_sequencer_reg(CHAR_MAP_SELECT);
+    set_sequencer_reg(CHAR_MAP_SELECT, (char_map_select & 0b11110011) | (csas << 2));
+    return;
+}
+
+uint8_t sequencer_reg_csas_get(void)
+{
+    uint8_t res = (get_sequencer_reg(CHAR_MAP_SELECT) >> 2) & 0b11;
+    if (sequencer_reg_check_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSAS2)) res |= 0b100;
+    return res;
+}
+
+void sequencer_reg_csbs_set(uint8_t csbs)
+{
+    assert(csbs <= 0b111);
+
+    if (csbs & 0b100)
+        sequencer_reg_set_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSBS2);
+    else 
+        sequencer_reg_clear_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSBS2);
+
+    csbs &= 0b11;
+
+    uint8_t char_map_select = get_sequencer_reg(CHAR_MAP_SELECT);
+    set_sequencer_reg(CHAR_MAP_SELECT, (char_map_select & 0b11111100) | csas);
+    return;
+}
+
+uint8_t sequencer_reg_csbs_get(void)
+{
+    uint8_t res = get_sequencer_reg(CHAR_MAP_SELECT) & 0b11;
+    if (sequencer_reg_check_bit(CHAR_MAP_SELECT, CHAR_MAP_SELECT_CSBS2)) res |= 0b100;
+    return res;
+}
+
+uint8_t get_attr_ctrl_reg(enum Attr_ctrl_register reg)
+{
+    inb(INP_STATUS_1_REG_READ);
+    outb(ATTR_ADDR_DATA_REG, reg & 0b11111);
+    return inb(ATTR_DATA_READ_REG);
+}
+
+void set_attr_ctrl_reg(enum Attr_ctrl_register reg, uint8_t value)
+{
+    assert(reg >= PALETTE_0 && reg <= CLR_SELECT);
+
+    inb(INP_STATUS_1_REG_READ);
+    outb(ATTR_ADDR_DATA_REG, reg & 0b11111);
+    outb(ATTR_ADDR_DATA_REG, value);
+}
+
+void attr_ctrl_reg_set_bit(enum Attr_ctrl_register reg, uint8_t bitno)
+{
+    assert(reg >= PALETTE_0 && reg <= CLR_SELECT);
+
+    uint8_t value = get_attr_ctrl_reg(reg);
+    set_attr_ctrl_reg(reg, value | (1 << bitno));
+    return;
+}
+
+void attr_ctrl_reg_clear_bit(enum Attr_ctrl_register reg, uint8_t bitno)
+{
+    assert(reg >= PALETTE_0 && reg <= CLR_SELECT);
+    
+    uint8_t value = get_attr_ctrl_reg(reg);
+    set_attr_ctrl_reg(reg, value & (~(1 << bitno)));
+    return;
+}
+
+bool attr_ctrl_reg_check_bit(enum Attr_ctrl_register reg, uint8_t bitno)
+{
+    assert(reg >= PALETTE_0 && reg <= CLR_SELECT);
+
+    return (get_attr_ctrl_reg(reg) & (1 << bitno));
+}
+
+void attr_ctrl_plt_reg_int_ind_set(uint8_t pltno, uint8_t ind)
+{
+    assert(pltno <= 0x0F);
+    assert(ind <= 0b11111);
+    
+    enum Attr_ctrl_register reg = PALETTE_0 + pltno;
+    uint8_t plt = get_attr_ctrl_reg(reg);
+    set_attr_ctrl_reg(reg, (plt & 0b11000000) | ind);
+    return;
+}
+
+uint8_t attr_ctrl_plt_reg_int_ind_get(uint8_t pltno)
+{
+    assert(pltno <= 0x0F);
+
+    enum Attr_ctrl_register reg = PALETTE_0 + pltno;
+    return get_attr_ctrl_reg(reg) & 0b111111;
+}
+
+void attr_ctrl_reg_hor_pxl_pan_set(uint8_t psc)
+{
+    assert(psc <= 0b1111);
+
+    uint8_t hor_pxl_pan = get_attr_ctrl_reg(HOR_PXL_PAN);
+    set_attr_ctrl_reg(HOR_PXL_PAN, (hor_pxl_pan & 0b11110000) | psc);
+    return;
+}
+
+uint8_t attr_ctrl_reg_hor_pxl_pan_get(void)
+{
+    return get_attr_ctrl_reg(HOR_PXL_PAN) & 0b1111;
+}
+
+
+
--- itask/kern/vga_regs.h
+++ itask/kern/vga_regs.h
@@ -15,24 +15,148 @@
 
 enum Graphic_register
 {
-    SET_RESET            = 0x0,
-    ENABLE_SET_RESET     = 0x1,
-    COLOR_COMPARE        = 0x2,
-    DATA_ROTATE          = 0x3,
-    READ_MAP_SELECT      = 0x4, 
-    GRAPHICS_MODE        = 0x5,
-    MISC_GRAPHICS        = 0x6,
-    COLOR_LOL_DIDNT_CARE = 0x7,
-    BIT_MASK             = 0x8
+    SET_RESET          = 0x00,
+    ENABLE_SET_RESET   = 0x01,
+    CLR_CMP            = 0x02,
+    DATA_ROTATE        = 0x03,
+    READ_MAP_SELECT    = 0x04, 
+    GRAPHICS_MODE      = 0x05,
+    MISC_GRAPHICS      = 0x06,
+    CLR_LOL_DIDNT_CARE = 0x07,
+    BIT_MASK           = 0x08
 };
 
+#define GRAPHICS_MODE_RMODE     3U
+#define GRAPHICS_MODE_HOST_OE   4U
+#define GRAPHICS_MODE_SHIFT_REG 5U
+#define GRAPHICS_MODE_SHIFT_256 6U
+
+#define MISC_GRAPHICS_CHAIN_OE  1U
+#define MISC_GRAPHICS_ALPHA_DIS 0U
+
 uint8_t get_graphics_reg(enum Graphic_register reg);
-void set_graphics_reg(enum Graphic_register reg, uint8_t value);
+void    set_graphics_reg(enum Graphic_register reg, uint8_t value);
 
-void graphics_reg_set_reset_set_bit(uint8_t bitno);
-void graphics_reg_set_reset_clear_bit(uint8_t bitno);
+void graphics_reg_set_bit(enum Graphic_register reg, uint8_t bitno);
+void graphics_reg_clear_bit(enum Graphic_register reg, uint8_t bitno);
+bool graphics_reg_check_bit(enum Graphic_register reg, uint8_t bitno);
 
-void graphics_reg_enable_set_reset_set_bit(uint8_t bitno);
-void graphics_reg_enable_set_reset_clear_bit(uint8_t bitno);
+uint8_t graphics_reg_clr_cmp_get(void);
+void    graphics_reg_clr_cmp_set(uint8_t clr_cmp);
+
+// 0b00 - no op, 0b01 - AND, 0b10 - OR, 0b11 - XOR
+void    graphics_reg_data_rotate_logical_operation_set(uint8_t log_op);
+uint8_t graphics_reg_data_rotate_logical_operation_get(void);
+
+void    graphics_reg_data_rotate_rotate_count_set(uint8_t ct);
+uint8_t graphics_reg_data_rotate_rotate_count_get(void);
+
+void    graphics_reg_read_map_select_set(uint8_t rms);
+uint8_t graphics_reg_read_map_select_get(void);
+
+void    graphics_reg_graphics_mode_write_mode_set(uint8_t wmode);
+uint8_t graphics_reg_graphics_mode_write_mode_get(void);
+
+// 00b -- A0000h-BFFFFh (128K region) // 01b -- A0000h-AFFFFh (64K region)
+// 10b -- B0000h-B7FFFh (32K region)  // 11b -- B8000h-BFFFFh (32K region)
+void    graphics_reg_misc_graphics_mem_map_select_set(uint8_t mms);
+uint8_t graphics_reg_misc_graphics_mem_map_select_get(void);
+
+//---------------------
+// Sequencer Registers
+//---------------------
+
+enum Sequencer_register
+{
+    RESET           = 0x00,
+    CLOCKING_MODE   = 0x01,
+    MAP_MASK        = 0x02,
+    CHAR_MAP_SELECT = 0x03,
+    SEQ_MEM_MODE    = 0x04
+};
+
+#define RESET_SR 1
+#define RESET_AR 0
+
+#define CLOCKING_MODE_SD   5U
+#define CLOCKING_MODE_S4   4U
+#define CLOCKING_MODE_DCR  3U
+#define CLOCKING_MODE_SLR  2U
+#define CLOCKING_MODE_98DM 0U // 0 - 9 dots/char, 1 - 8 dots/char 
+
+#define CHAR_MAP_SELECT_CSAS2 5U
+#define CHAR_MAP_SELECT_CSBS2 4U
+
+#define SEQ_MEM_MODE_CHAIN_4 3U
+#define SEQ_MEM_MODE_OE_DIS  2U
+#define SEQ_MEM_MODE_EXT_MEM 1U
+
+uint8_t get_sequencer_reg(enum Sequencer_register reg);
+void    set_sequencer_reg(enum Sequencer_register reg, uint8_t value);
+
+void sequencer_reg_set_bit(enum Sequencer_register reg, uint8_t bitno);
+void sequencer_reg_clear_bit(enum Sequencer_register reg, uint8_t bitno);
+bool sequencer_reg_check_bit(enum Sequencer_register reg, uint8_t bitno);
+
+// 000b -- Select font residing at 0000h - 1FFFh 001b -- Select font residing at 4000h - 5FFFh
+// 010b -- Select font residing at 8000h - 9FFFh 011b -- Select font residing at C000h - DFFFh
+// 100b -- Select font residing at 2000h - 3FFFh 101b -- Select font residing at 6000h - 7FFFh
+// 110b -- Select font residing at A000h - BFFFh 111b -- Select font residing at E000h - FFFFh
+
+void    sequencer_reg_csas_set(uint8_t csas);
+uint8_t sequencer_reg_csas_get(void);
+
+void    sequencer_reg_csbs_set(uint8_t csbs);
+uint8_t sequencer_reg_csbs_get(void);
+
+//--------------------------------
+// Attribute Controller Registers
+//--------------------------------
+
+enum Attr_ctrl_register
+{
+    PALETTE_0      = 0x00,
+    PALETTE_1      = 0x01,
+    PALETTE_2      = 0x02,
+    PALETTE_3      = 0x03,
+    PALETTE_4      = 0x04,
+    PALETTE_5      = 0x05,
+    PALETTE_6      = 0x06,
+    PALETTE_7      = 0x07,
+    PALETTE_8      = 0x08,
+    PALETTE_9      = 0x09,
+    PALETTE_A      = 0x0A,
+    PALETTE_B      = 0x0B,
+    PALETTE_C      = 0x0C,
+    PALETTE_D      = 0x0D,
+    PALETTE_E      = 0x0E,
+    PALETTE_F      = 0x0F,
+    ATTR_MODE_CTRL = 0x10,
+    OVERSCAN_CLR   = 0x11,
+    CLR_PLANE_E    = 0x12,
+    HOR_PXL_PAN    = 0x13,
+    CLR_SELECT     = 0x14,
+};
+
+#define ATTR_MODE_CTRL_P54S  7
+#define ATTR_MODE_CTRL_8BIT  6
+#define ATTR_MODE_CTRL_PPM   5
+#define ATTR_MODE_CTRL_BLINK 3
+#define ATTR_MODE_CTRL_LGE   2
+#define ATTR_MODE_CTRL_MONO  1
+#define ATTR_MODE_CTRL_ATGE  0
+
+uint8_t get_attr_ctrl_reg(enum Attr_ctrl_register reg);
+void    set_attr_ctrl_reg(enum Attr_ctrl_register reg, uint8_t value);
+
+void attr_ctrl_reg_set_bit(enum Attr_ctrl_register reg, uint8_t bitno);
+void attr_ctrl_reg_clear_bit(enum Attr_ctrl_register reg, uint8_t bitno);
+bool attr_ctrl_reg_check_bit(enum Attr_ctrl_register reg, uint8_t bitno);
+
+void    attr_ctrl_reg_plt_int_ind_set(uint8_t pltno, uint8_t ind);
+uint8_t attr_ctrl_reg_plt_int_ind_get(uint8_t pltno);
+
+void    attr_ctrl_reg_hor_pxl_pan_set(uint8_t psc);
+uint8_t attr_ctrl_reg_hor_pxl_pan_get(void);
 
 #endif // JOS_KERN_VGA_REGS_H
